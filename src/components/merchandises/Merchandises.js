4bcf88809e92657924be6fceb2962c1e09e8fd2e
Jalamang <Jalamangjanka@gmail.com>
2022-07-28 14:33:13 -0400

    hard coded authentication for privated and restricted routes

--- src/components/merchandises/Merchandises.js
+++ src/components/merchandises/Merchandises.js
@@ -1,51 +1,71 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import ProductCard from '../productCard/ProductCard';
-import SearchComponent from '../searchComponent/SearchComponent';
-import "./Merchandises.scss"
+import React from "react";
+import { useState, useEffect } from "react";
+import ProductCard from "../productCard/ProductCard";
+import SearchComponent from "../searchComponent/SearchComponent";
+import "./Merchandises.scss";
 const Merchandises = () => {
+  const URL = "http://localhost:3309/merchandises/";
+  // const API = process.env.REACT_APP_API_URL;
 
-    const URL = 'http://localhost:3309/merchandises/'
-    // const API = process.env.REACT_APP_API_URL;
+  const [products, setProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const [products, setProducts] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
+  // functions
+  useEffect(() => {
+    // reach out to the backend
+    fetch(URL)
+      .then((response) => response.json()) // get our vendors
+      .then((data) => {
+        console.log(data);
+        setProducts(data.merchandises); // update our vendors hook with the new data
+      });
+  }, []); // empty array means run on mount
 
-    // functions
-    useEffect(() => {
-        // reach out to the backend
-        fetch(URL)
-        .then(response => response.json()) // get our vendors
-        .then(data => { 
-            console.log(data)
-            setProducts(data.merchandises);// update our vendors hook with the new data
-        })
+  let filteredProducts = products;
+  if (searchTerm) {
+    filteredProducts = products.filter((product) => {
+      const productCategory = product.category.toLowerCase();
+      const searchTermToLowerCase = searchTerm.toLowerCase();
+      return productCategory.includes(searchTermToLowerCase);
+    });
+  }
+  return (
+    <>
+      <SearchComponent searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <div className="searchTerm">e.g Meat, Vegetable, Fish</div>
 
-    }, []); // empty array means run on mount
+      <div className="row">
+        <div className="location-title ">
+          <h4>Find merchandises by location</h4>
+          <h6>Browse open merchandises in different locations</h6>
+        </div>
+        <div className=" merchandises-row">
+          <button className="tile-filter">All locations</button>
 
+          <button className="tile-filter">Brikama</button>
 
+          <button className="tile-filter">Farafenni</button>
 
-    let filteredProducts = products;
-    if(searchTerm){
-        filteredProducts = products.filter(product => {
-           const productCategory = product.category.toLowerCase();
-           const searchTermToLowerCase = searchTerm.toLowerCase();
-            return productCategory.includes(searchTermToLowerCase)
-    })
-}
-  return (<>
-    <SearchComponent searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-    <div className='searchTerm'>e.g Meat, Vegetable, Fish</div>
-     <div className='merchandises'>
-    {filteredProducts.map((product) => {
-            return (
-                <ProductCard product={product} key={product.merchan_id} />
-            )
-           })}
-     </div>
-     {filteredProducts.length === 0 && <div className="merchandises__noResults">No search result for this category </div>}
-     </>
-  )
-}
+          <button className="tile-filter">Basse</button>
 
-export default Merchandises
+          <button className="tile-filter">Soma &amp; Pakalinding</button>
+
+          <button className="tile-filter">Badibou</button>
+        </div>
+      </div>
+
+      <div className="merchandises">
+        {filteredProducts.map((product) => {
+          return <ProductCard product={product} key={product.merchan_id} />;
+        })}
+      </div>
+      {filteredProducts.length === 0 && (
+        <div className="merchandises__noResults">
+          No search result for this category{" "}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Merchandises;
--- src/components/routing/Routing.js
+++ src/components/routing/Routing.js
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Routes, Route, BrowserRouter, Outlet, Navigate } from 'react-router-dom'
 import React from 'react'
 import Vendors from '../Vendors/Vendors'
 import Home from '../../pages/home/Home'
@@ -11,22 +11,35 @@ import EditVendor from '../editVendor/EditVendor'
 import NewProduct from '../newProduct/NewProduct'
 import Login from '../loginLogout/Login'
 
+const PrivateRoutes = () => {
+const isAuth = false
+return <>{isAuth ? <Outlet /> : <Navigate to="/vendors/login" />}</>
+}
+
+const RestrictedRoutes = () => {
+  const isAuth = false
+  return <>{!isAuth ? <Outlet /> : <Navigate to="/vendors" />}</>
+  }
 const Routing = () => {
   return (<>
     <BrowserRouter>
     <Navbar/>
     <Routes>
         <Route exact path='/' element={<Home />} /> 
+        
+        <Route element={<PrivateRoutes />}>
         <Route path='/vendors' element={<Vendors />} /> 
-        <Route path='/merchandises' element={<Merchandises />} /> 
+        <Route path='/merchandises' element={<Merchandises />} />
+        <Route path='/merchandises/new' element={<NewProduct/>} />  
+        </Route>
+        <Route element={<RestrictedRoutes />}>
         <Route path='/merchandises/:id' element={<ProductDetail/>} /> 
-        <Route path='/merchandises/new' element={<NewProduct/>} /> 
         <Route path='/merchandises/:id/edit' element={<NewProduct/>} /> 
         <Route path='/vendors/:id' element={<VendorDetails/>} /> 
         <Route path='/vendors/:id/edit' element={<EditVendor/>} /> 
         <Route path='/vendors/new' element={<NewVendor/>} /> 
         <Route path='/vendors/login' element={<Login/>} /> 
-
+        </Route>
         
     </Routes>
     </BrowserRouter>
